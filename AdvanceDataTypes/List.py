895299262b1181b5bee726402939622f54f23762
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2025-03-17 18:27:11 +0530

    AdvanceDataType/list.py

---
+++ AdvanceDataTypes/List.py
@@ -0,0 +1,28 @@
+# How to create the list
+
+li1 = [10,20,10, 'Code', True, 66.7]
+print(li1, type(li1))
+
+
+print(li1[0])
+print(li1[1])
+print(li1[2])
+print(li1[3])
+print(li1[4])
+
+li1[3] = 'MyCode'
+print("After Update: ",li1)
+
+
+'''
+1. In List we can store Homogeneous (Data with similar Data Type) as well as
+Heterogeneous (Data with different Data Type) Type of Data.
+
+2. List is an Ordered Collection of Data. List Maintains order insertion in the output
+thats why we call it as ordered collection of Data.
+
+3. In List we can Store duplicate values.
+
+4. Lists are Mutable means once we declare the list we can modify it.
+
+'''
