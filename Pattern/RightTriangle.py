2310c9cb02933c1e224e7b26b5369f0676ed1f68
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2025-03-17 19:22:03 +0530

    right Triangle Number

--- Pattern/RightTriangle.py
+++ Pattern/RightTriangle.py
@@ -12,3 +12,26 @@ for i in range(1, row+1):
     for i in range(i):
         print("*", end = " ")
     print()
+
+# output: 
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+
+
+#Number printing
+# row = int(input('Enter Rows: '))
+# for i in range(1, row+1):
+#     for j in range(1, i + 1):
+#         print(j, end = " ")
+#     print()
+
+# output
+# 1 
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+# 1 2 3 4 5 6
