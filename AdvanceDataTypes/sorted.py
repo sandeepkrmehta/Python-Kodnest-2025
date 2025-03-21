65df7fa62a7d86918a85d03229f5e565fa2521f2
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2025-03-21 17:45:26 +0530

    sorted method

---
+++ AdvanceDataTypes/sorted.py
@@ -0,0 +1,16 @@
+'''
+-> sor() will sort original list.
+'''
+li = [11,5,44,12,9]
+li.sort()
+print(li)   # [5, 9, 11, 12, 44]
+
+
+'''
+-> Sorted() created new sorted list
+'''
+li1 = [44,5,11,99]
+
+li2 = sorted(li1)
+
+print(li2)   # [5, 11, 44, 99]
