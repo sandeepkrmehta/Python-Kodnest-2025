36173417f4e40a554d15c977217fac60262c805b
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2025-03-21 17:41:39 +0530

    intro

---
+++ ControlConstructs/intro.py
@@ -0,0 +1,23 @@
+#for loop (initialization, condition check, ++/--)
+for i in range(3):
+    print("I see myself")
+print('In the Mirror')
+
+#WAC to print numbers from 0-10
+for i in range(11):
+    print(i, end = " ")
+
+print()
+#WAC to print numbers from 5-15
+for i in range(5,16):
+    print(i, end = " ")
+
+print()
+#WAC to print all even numbers from 1-10
+for i in range(2,11,2):
+    print(i, end = " ")
+
+print()
+#WAC to print all odd numbers from 1-10
+for i in range(1,11,2):
+    print(i, end = " ")
