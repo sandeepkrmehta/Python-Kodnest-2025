170df8e1ed8319f2158ce39d58c26765a315978e
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2025-03-24 17:49:20 +0530

    Opps Concept started

---
+++ OoPS/practice.py
@@ -0,0 +1,15 @@
+class Employee:
+    name = 'Sandeep'
+    age = 23
+
+    def code():
+        print('Employee is coding')
+
+e1 = Employee()
+e2 = Employee()
+
+'''
+1. Method which are present inside the class are called as Instance(object) Methods.
+2. For Instance Methods self parameter is must.
+
+'''
