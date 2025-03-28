2b3f0c5a5f5f9ea4ded584fcdd132a9ff39fa168
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2025-03-28 17:59:37 +0530

    Method Overidding

---
+++ pythonPractice/MethodOveridding.py
@@ -0,0 +1,25 @@
+class Parent:
+    def cook(self):
+        print("Cooking Biryani")
+    def learn(self):
+        print("Learning Maths")
+        
+class Child(Parent):
+    def play(self):
+        print('Playing Cricket')
+    def learn(self):
+        print("Learning Python")
+c = Child()
+c.cook()
+
+
+'''
+The Method which is derived from parent class and used as it is in child class, we call it as 
+Inherited Method.
+
+The Method which is only available for child class and not for parent class, such methods are
+called as Specialized Methods.
+
+The Methods which are derived from parent class and Modified into child class as per
+child class requriment , such methods are called as Overidden Methods.
+'''
