e08dbe76ba565acdf4c75571d82167dc7e9c4cd8
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2025-04-02 17:38:16 +0530

    Defined

--- pythonPractice/private.py
+++ pythonPractice/private.py
@@ -1,3 +1,5 @@
+'''Privates variable must be accessed inside the same class.'''
+
 class Demo1:
     def __init__(self):
         self.__name = 'AK'  # Private variable
@@ -7,3 +9,17 @@ class Demo1:
 
 d1 = Demo1()
 #print(d1.__name)  #error
+
+'''
+Public: When we want to access variables anywhere in the code.
+Protected: When we want to access variables inside the same class and inside child class.
+Private: When we want to access variables inside the same class.
+
+Access Declearations:
+
+public a = 10
+protected: _a = 10
+private: __a = 10
+
+'''
+
